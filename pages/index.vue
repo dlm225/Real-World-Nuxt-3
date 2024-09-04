33b2fd05385d0ce986c560c761ef601e4b7c8d37
dlm225 <david.morgan2@gmail.com>
2024-09-04 10:41:28 -0400

    chore: Update devtools configuration and add compatibility date

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import type { Post } from "@/data/posts";
+import { getPosts, getPostsUrl } from "@/data/posts";
+
+const { data: posts } = await useFetch(getPostsUrl());
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <h1>Nuxt.js Examples</h1>
+    <div>
+      <PostCard v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+  </main>
 </template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/colors.scss";
+.card {
+  padding: 15px;
+  border: 1px solid colors.$green-dark;
+  border-radius: 5px;
+  margin-bottom: 25px;
+  font-size: 12pt;
+}
+
+.card .title {
+  font-size: 20pt;
+}
+
+.card .category {
+  margin: 5px 0px;
+}
+</style>
